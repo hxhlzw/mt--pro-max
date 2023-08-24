ef79026b1891dcb79bb52a44395275a21298f6f2
hxh <2097500236@qq.com>
2023-08-24 15:08:51 +0800

    feat: pinia 持久化 vant 配置 axios 拦截

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
+import persist from 'pinia-plugin-persistedstate'
 const app = createApp(App)
-
-app.use(createPinia())
+app.use(createPinia().use(persist))
 app.use(router)
+// 样式全局使⽤
+import 'vant/lib/index.css'
+import './styles/main.scss'
 
 app.mount('#app')
